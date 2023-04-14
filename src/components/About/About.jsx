27e9d781bb0cdbdfa8dc8297d0bfd0f28a032a3d
Anthony Cox <double_a1420@yahoo.com.com>
2023-04-13 23:47:29 -0700

    another update commit

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,59 +1,74 @@
 import React from 'react';
 import styles from './About.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faRocket, faBuilding, faTrophy, faHandshake, faGlobe, faBullseye, faLightbulb } from '@fortawesome/free-solid-svg-icons';
 
 const About = () => {
   return (
     <div className={styles.about}>
-      <header
-        className={styles.header}
-      >
+      <header className={styles.header}>
         <h1>About Our Business</h1>
       </header>
       
       <section className={styles.introduction}>
-        <h2>Our Mission and Vision</h2>
-        <p>
-        Our mission is to empower businesses and individuals by providing innovative and cutting-edge solutions tailored to their unique needs. 
-        We are dedicated to driving success through exceptional customer service, unparalleled expertise, and continuous improvement.
-         Our vision is to become the leading provider of our services, setting the industry standard in quality and performance, while fostering a culture of collaboration, growth, and sustainability.
-          
-        </p>
+        <div className={styles.iconWrapper}>
+          <FontAwesomeIcon icon={faBullseye} className={styles.icon} />
+        </div>
+        <div className={styles.missionVision}>
+          <h2>Our Mission and Vision</h2>
+          <p>
+            <strong>Mission:</strong> To provide exceptional drywall finishing services for businesses and individuals, using innovative techniques and the highest quality materials. We are dedicated to driving success through exceptional customer service, unparalleled expertise, and continuous improvement.
+          </p>
+          <p>
+            <strong>Vision:</strong> To become the leading provider of drywall finishing services, setting the industry standard in quality and performance, while fostering a culture of collaboration, growth, and sustainability.
+          </p>
+        </div>
       </section>
       
       <section className={styles.history}>
         <h2>Our History and Milestones</h2>
-        <p>
-         Our journey began in [2021] with a small team of passionate professionals committed to delivering exceptional results. 
-         Since then, we have grown exponentially, expanding our range of services and establishing a strong presence in the market. 
-         Some of our most notable milestones include:
-
-        [YEAR]: Launched our first product, revolutionizing the way businesses approach [SPECIFIC AREA].
-        [YEAR]: Expanded our team and relocated to a state-of-the-art facility, enabling us to better serve our growing clientele.
-        [YEAR]: Received prestigious industry awards, including the [AWARD NAME], recognizing our commitment to excellence and innovation.
-        [YEAR]: Established strategic partnerships with industry leaders, expanding our reach and enhancing our capabilities.
-        [YEAR]: Surpassed a significant milestone, serving over [NUMBER] clients across [NUMBER] countries, cementing our reputation as a trusted and reliable partner.
-          
-        </p>
+        <ul className={styles.milestoneList}>
+          <li>
+            <FontAwesomeIcon icon={faRocket} />
+            [2018]: Started Supreme Drywall Solutions, offering professional drywall finishing services for residential and commercial clients.
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faBuilding} />
+            [2020]: Expanded our team and relocated to a state-of-the-art facility, enabling us to better serve our growing clientele.
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faTrophy} />
+            [2021]: Received prestigious industry awards, including the [AWARD NAME], recognizing our commitment to excellence and innovation in drywall finishing.
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faHandshake} />
+            [2022]: Established strategic partnerships with industry leaders, expanding our reach and enhancing our capabilities.
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faGlobe} />
+            [2023]: Surpassed a significant milestone, serving over [1000] clients across [2] countries, cementing our reputation as a trusted and reliable partner in drywall finishing.
+          </li>
+        </ul>
       </section>
       
       <section className={styles.testimonials}>
         <h2>What People Say About Us</h2>
         <blockquote>
-         "Working with [Supreme Drywall Solutions] has been a game-changer for our company. Their expertise, dedication, 
-         and innovative solutions have helped us scale our business and achieve impressive results.
-         We couldn't be happier with our partnership and highly recommend them to anyone looking for a trusted service provider."
+          "Working with Supreme Drywall Solutions has been a mind-blowing experience. Their expertise, dedication, and innovative solutions have transformed our home, achieving impressive results with their drywall finishing work. We couldn't be happier with our decision to choose them for our home remodel. Now we can confidently have our family and friends over. Thank you, SDS! We will be using you again in the future."
         </blockquote>
       </section>
       
       <section className={styles.cta}>
-        <h2>Ready to Learn More?</h2>
-        <p>
-        Discover the difference that [S.D.S] can make for your business.
-         Explore our range of services, check out our case studies, 
-         or get in touch with our team of experts to discuss your unique needs. 
-         Let us help you unlock your full potential and achieve remarkable success. Contact us today!
-
-        </p>
+        <div className={styles.iconWrapper}>
+          <FontAwesomeIcon icon={faLightbulb} className={styles.icon} />
+        </div>
+        <div className={styles.readyToLearn}>
+          <h2>Ready to Learn More?</h2>
+          <p>
+            Discover the difference that Supreme Drywall Solutions can make in your home. Explore our range of services, check out our case studies, or get in touch with our team of experts to discuss your unique needs. Let us help you unlock your full potential and achieve remarkable home designs with our drywall finishing expertise.
+          </p>
+          <button className={styles.contactButton}>Contact Us Today!</button>
+        </div>
       </section>
     </div>
   );
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
       <div className={styles.heroContent}>
         <h1 className={styles.heroTitle}>Supreme Drywall Solutions</h1>
         <p className={styles.heroSubtitle}>Quality drywall services for your home</p>
-        <a href="https://example.com" target="_blank" rel="noopener noreferrer">
+        <a href="https://forms.gle/NSPi8WUCNoY7jmnX8" target="_blank" rel="noopener noreferrer">
         <button className={styles.ctaButton}>Get an Estimate</button>
         </a>
       </div>
